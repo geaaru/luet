198254bd0c6e6053ae85986ba6816b15a233b225
Daniele Rondina <geaaru@sabayonlinux.org>
2021-11-05 12:36:07 +0100

    tests/integration/20_plugin.sh: Permit to run test on luet env

--- tests/integration/20_plugin.sh
+++ tests/integration/20_plugin.sh
@@ -5,9 +5,24 @@ export PATH=$PATH:$ROOT_DIR/tests/fixtures/plugin
 
 
 oneTimeSetUp() {
-export tmpdir="$(mktemp -d)"
-export EVENT_FILE=$tmpdir/events.txt
-export PAYLOAD_FILE=$tmpdir/payloads.txt
+  export tmpdir="$(mktemp -d)"
+  export EVENT_FILE=$tmpdir/events.txt
+  export PAYLOAD_FILE=$tmpdir/payloads.txt
+  export tmpdir="$(mktemp -d)"
+  cat <<EOF > $tmpdir/luet-build.yaml
+general:
+  debug: true
+logging:
+  enable_emoji: false
+  color: false
+system:
+  rootfs: $tmpdir/testrootfs
+  database_path: "/"
+  database_engine: "memory"
+config_from_host: true
+repos_confdir:
+  - "$tmpdir/etc/luet/repos.conf.d"
+EOF
 }
 
 oneTimeTearDown() {
@@ -16,7 +31,10 @@ oneTimeTearDown() {
 
 testBuild() {
     mkdir $tmpdir/testbuild
-    luet build --plugin test-foo --tree "$ROOT_DIR/tests/fixtures/templatedfinalizers" --destination $tmpdir/testbuild --compression gzip --all
+    luet build --config $tmpdir/luet-build.yaml \
+      --plugin test-foo \
+      --tree "$ROOT_DIR/tests/fixtures/templatedfinalizers" \
+      --destination $tmpdir/testbuild --compression gzip --all
     buildst=$?
     assertEquals 'builds successfully' "$buildst" "0"
     assertContains 'event file contains corresponding event' "$(cat $EVENT_FILE)" 'package.pre.build'
@@ -25,13 +43,15 @@ testBuild() {
 
 testRepo() {
     assertTrue 'no repository' "[ ! -e '$tmpdir/testbuild/repository.yaml' ]"
-    luet --plugin test-foo create-repo --tree "$ROOT_DIR/tests/fixtures/templatedfinalizers" \
-    --output $tmpdir/testbuild \
-    --packages $tmpdir/testbuild \
-    --name "test" \
-    --descr "Test Repo" \
-    --urls $tmpdir/testrootfs \
-    --type disk > /dev/null
+    luet create-repo --config $tmpdir/luet-build.yaml \
+      --plugin test-foo \
+      --tree "$ROOT_DIR/tests/fixtures/templatedfinalizers" \
+      --output $tmpdir/testbuild \
+      --packages $tmpdir/testbuild \
+      --name "test" \
+      --descr "Test Repo" \
+      --urls $tmpdir/testrootfs \
+      --type disk > /dev/null
 
     createst=$?
     assertEquals 'create repo successfully' "$createst" "0"
@@ -48,6 +68,8 @@ system:
   database_path: "/"
   database_engine: "boltdb"
 config_from_host: true
+repos_confdir:
+  - "$tmpdir/etc/luet/repos.conf.d"
 repositories:
    - name: "main"
      type: "disk"
