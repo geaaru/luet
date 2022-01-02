6109e06bcf651ef1a6b3d27ca96826515dd0a748
Daniele Rondina <geaaru@funtoo.org>
2022-01-02 12:25:58 +0100

    benchmark_test: Align test to ginkgo/v2 (before migrate to gomega/measure)

--- pkg/solver/benchmark_test.go
+++ pkg/solver/benchmark_test.go
@@ -255,7 +255,7 @@ var _ = Describe("Solver Benchmarks", func() {
 			Ω(runtime.Seconds()).Should(BeNumerically("<", 120), "Install() shouldn't take too long.")
 		}, 1)
 
-		PMeasure("it should be fast in resolution from a 50000 dataset with upgrade universe", func(b Benchmarker) {
+		Measure("it should be fast in resolution from a 50000 dataset with upgrade universe", func(b Benchmarker) {
 			runtime := b.Time("runtime", func() {
 				for i := 0; i < 2; i++ {
 					C := pkg.NewPackage("C", strconv.Itoa(i), []*pkg.DefaultPackage{}, []*pkg.DefaultPackage{})
