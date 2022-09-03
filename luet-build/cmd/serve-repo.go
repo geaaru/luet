7d8c95e4b6f29dd73bb7dc4e265cb2805af52298
Daniele Rondina <geaaru@funtoo.org>
2022-09-03 14:36:57 +0200

    luet-build server-repo: Add logging of the reqs

--- luet-build/cmd/serve-repo.go
+++ luet-build/cmd/serve-repo.go
@@ -14,24 +14,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type HandlerHttpLogger struct {
+	Config  *cfg.LuetConfig
+	Handler http.Handler
+}
+
+func (h *HandlerHttpLogger) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	Debug("Processing ", req.URL)
+	h.Handler.ServeHTTP(resp, req)
+}
+
 func newServerRepoCommand(config *cfg.LuetConfig) *cobra.Command {
 
 	var serverepoCmd = &cobra.Command{
 		Use:   "serve-repo",
 		Short: "Embedded micro-http server",
 		Long:  `Embedded mini http server for serving local repositories`,
-		PreRun: func(cmd *cobra.Command, args []string) {
-			config.Viper.BindPFlag("dir", cmd.Flags().Lookup("dir"))
-			config.Viper.BindPFlag("address", cmd.Flags().Lookup("address"))
-			config.Viper.BindPFlag("port", cmd.Flags().Lookup("port"))
-		},
 		Run: func(cmd *cobra.Command, args []string) {
 
 			dir := config.Viper.GetString("dir")
 			port := config.Viper.GetString("port")
 			address := config.Viper.GetString("address")
 
-			http.Handle("/", http.FileServer(http.Dir(dir)))
+			handler := &HandlerHttpLogger{Config: config}
+			handler.Handler = http.FileServer(http.Dir(dir))
+			http.Handle("/", handler)
 
 			Info("Serving ", dir, " on HTTP port: ", port)
 			Fatal(http.ListenAndServe(address+":"+port, nil))
@@ -42,9 +49,13 @@ func newServerRepoCommand(config *cfg.LuetConfig) *cobra.Command {
 	if err != nil {
 		Fatal(err)
 	}
+
 	serverepoCmd.Flags().String("dir", path, "Packages folder (output from build)")
 	serverepoCmd.Flags().String("port", "9090", "Listening port")
 	serverepoCmd.Flags().String("address", "0.0.0.0", "Listening address")
+	config.Viper.BindPFlag("dir", serverepoCmd.Flags().Lookup("dir"))
+	config.Viper.BindPFlag("address", serverepoCmd.Flags().Lookup("address"))
+	config.Viper.BindPFlag("port", serverepoCmd.Flags().Lookup("port"))
 
 	return serverepoCmd
 }
