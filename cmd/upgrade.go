28e9a82925b2f2c98727f5cfa312151a314feb47
Daniele Rondina <geaaru@funtoo.org>
2023-01-29 00:39:12 +0100

    cmd/upgrade: Move to new implementation

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -1,117 +1,142 @@
-// Copyright © 2019-2021 Ettore Di Giacinto <mudler@gentoo.org>
-//                       Daniele Rondina <geaaru@sabayonlinux.org>
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, see <http://www.gnu.org/licenses/>.
+/*
+Copyright © 2019-2023 Macaroni OS Linux
+See AUTHORS and LICENSE for the license details and contributors.
+*/
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	cmdrepo "github.com/geaaru/luet/cmd/repo"
 	"github.com/geaaru/luet/cmd/util"
-	. "github.com/geaaru/luet/pkg/config"
-	config "github.com/geaaru/luet/pkg/config"
-	installer "github.com/geaaru/luet/pkg/installer"
+	cfg "github.com/geaaru/luet/pkg/config"
 	. "github.com/geaaru/luet/pkg/logger"
-	"github.com/geaaru/luet/pkg/solver"
+	"github.com/geaaru/luet/pkg/subsets"
+	installer "github.com/geaaru/luet/pkg/v2/installer"
+	"golang.org/x/sync/semaphore"
 
 	"github.com/spf13/cobra"
 )
 
-func newUpgradeCommand(cfg *config.LuetConfig) *cobra.Command {
+func newUpgradeCommand(config *cfg.LuetConfig) *cobra.Command {
 
 	var upgradeCmd = &cobra.Command{
 		Use:     "upgrade",
 		Short:   "Upgrades the system",
 		Aliases: []string{"u"},
 		PreRun: func(cmd *cobra.Command, args []string) {
-			util.BindSolverFlags(cmd)
-			cfg.Viper.BindPFlag("force", cmd.Flags().Lookup("force"))
-			cfg.Viper.BindPFlag("yes", cmd.Flags().Lookup("yes"))
+			config.Viper.BindPFlag("force", cmd.Flags().Lookup("force"))
+			config.Viper.BindPFlag("yes", cmd.Flags().Lookup("yes"))
 		},
-		Long: `Upgrades packages in parallel`,
+		Long: `Upgrades packages installed.`,
 		Run: func(cmd *cobra.Command, args []string) {
 
-			repos := installer.Repositories{}
-			for _, repo := range LuetCfg.SystemRepositories {
-				if !repo.Enable {
-					continue
-				}
+			Info("Luet version", util.Version())
 
-				r := installer.NewSystemRepository(repo)
-				repos = append(repos, r)
-			}
-
-			force := LuetCfg.Viper.GetBool("force")
+			force := config.Viper.GetBool("force")
 			nodeps, _ := cmd.Flags().GetBool("nodeps")
-			full, _ := cmd.Flags().GetBool("full")
-			universe, _ := cmd.Flags().GetBool("universe")
-			clean, _ := cmd.Flags().GetBool("clean")
-			sync, _ := cmd.Flags().GetBool("sync")
-			yes := LuetCfg.Viper.GetBool("yes")
+			yes := config.Viper.GetBool("yes")
 			downloadOnly, _ := cmd.Flags().GetBool("download-only")
+			pretend, _ := cmd.Flags().GetBool("pretend")
+			preserveSystem, _ := cmd.Flags().GetBool("preserve-system-essentials")
+			finalizerEnvs, _ := cmd.Flags().GetStringArray("finalizer-env")
 			skipFinalizers, _ := cmd.Flags().GetBool("skip-finalizers")
 			syncRepos, _ := cmd.Flags().GetBool("sync-repos")
 
-			util.SetSystemConfig()
-			opts := util.SetSolverConfig()
+			// TODO: Move this inside the ArtifactManager or
+			//       to a common function.
+			if syncRepos {
+				waitGroup := &sync.WaitGroup{}
+				sem := semaphore.NewWeighted(int64(config.GetGeneral().Concurrency))
+				ctx := context.TODO()
 
-			Debug("Solver", opts.CompactString())
+				defer waitGroup.Wait()
+
+				var ch chan util.ChannelRepoOpRes = make(
+					chan util.ChannelRepoOpRes,
+					config.GetGeneral().Concurrency,
+				)
+				// Using new way
+				nOps := 0
+
+				for idx, repo := range config.SystemRepositories {
+					if repo.Enable {
+						waitGroup.Add(1)
+						go cmdrepo.ProcessRepository(
+							&config.SystemRepositories[idx], config, ch, force, sem,
+							waitGroup, &ctx)
+						nOps++
+					}
+				}
+
+				res := 0
+				if nOps > 0 {
+					for i := 0; i < nOps; i++ {
+						resp := <-ch
+						if resp.Error != nil && !force {
+							res = 1
+							Error("Error on update repository " + resp.Repo.Name + ": " +
+								resp.Error.Error())
+						}
+					}
+				} else {
+					fmt.Println("No repositories candidates found.")
+				}
+
+				if res != 0 {
+					os.Exit(res)
+				}
+
+			}
 
 			// Load config protect configs
-			installer.LoadConfigProtectConfs(LuetCfg)
+			installer.LoadConfigProtectConfs(config)
+			// Load subsets defintions
+			subsets.LoadSubsetsDefintions(config)
+			// Load subsets config
+			subsets.LoadSubsetsConfig(config)
 
-			inst := installer.NewLuetInstaller(installer.LuetInstallerOptions{
-				Concurrency:                 LuetCfg.GetGeneral().Concurrency,
-				SolverOptions:               *LuetCfg.GetSolverOptions(),
-				Force:                       force,
-				FullUninstall:               full,
-				NoDeps:                      nodeps,
-				SolverUpgrade:               universe,
-				RemoveUnavailableOnUpgrade:  clean,
-				UpgradeNewRevisions:         sync,
-				PreserveSystemEssentialData: true,
-				Ask:                         !yes,
-				DownloadOnly:                downloadOnly,
-				SkipFinalizers:              skipFinalizers,
-				SyncRepositories:            syncRepos,
-			})
-			inst.Repositories(repos)
-
-			system := &installer.System{
-				Database: LuetCfg.GetSystemDB(),
-				Target:   LuetCfg.GetSystem().Rootfs,
+			// Load finalizer runtime environments
+			err := util.SetCliFinalizerEnvs(finalizerEnvs)
+			if err != nil {
+				Fatal(err.Error())
 			}
-			if err := inst.Upgrade(system); err != nil {
+
+			aManager := installer.NewArtifactsManager(config)
+			defer aManager.Close()
+
+			opts := &installer.InstallOpts{
+				Force:                       force,
+				NoDeps:                      nodeps,
+				PreserveSystemEssentialData: preserveSystem,
+				Ask:                         !yes,
+				SkipFinalizers:              skipFinalizers,
+				Pretend:                     pretend,
+				DownloadOnly:                downloadOnly,
+			}
+			if err := aManager.Upgrade(opts, config.GetSystem().Rootfs); err != nil {
 				Fatal("Error: " + err.Error())
 			}
 		},
 	}
 
-	upgradeCmd.Flags().String("solver-type", "", "Solver strategy ( Defaults none, available: "+solver.AvailableResolvers+" )")
-	upgradeCmd.Flags().Float32("solver-rate", 0.7, "Solver learning rate")
-	upgradeCmd.Flags().Float32("solver-discount", 1.0, "Solver discount rate")
-	upgradeCmd.Flags().Int("solver-attempts", 9000, "Solver maximum attempts")
-	upgradeCmd.Flags().Bool("force", false, "Force upgrade by ignoring errors")
-	upgradeCmd.Flags().Bool("nodeps", false, "Don't consider package dependencies (harmful! overrides checkconflicts and full!)")
-	upgradeCmd.Flags().Bool("full", false, "Attempts to remove as much packages as possible which aren't required (slow)")
-	upgradeCmd.Flags().Bool("universe", false, "Use ONLY the SAT solver to compute upgrades (experimental)")
-	upgradeCmd.Flags().Bool("clean", false, "Try to drop removed packages (experimental, only when --universe is enabled)")
-	upgradeCmd.Flags().Bool("sync", false, "Upgrade packages with new revisions (experimental)")
-	upgradeCmd.Flags().Bool("solver-concurrent", false, "Use concurrent solver (experimental)")
-	upgradeCmd.Flags().BoolP("yes", "y", false, "Don't ask questions")
-	upgradeCmd.Flags().Bool("download-only", false, "Download only")
-	upgradeCmd.Flags().Bool("skip-finalizers", false,
+	flags := upgradeCmd.Flags()
+
+	flags.BoolP("pretend", "p", false,
+		"simply display what *would* have been upgraded if --pretend weren't used")
+	flags.Bool("nodeps", false, "Don't consider package dependencies (harmful!)")
+	flags.Bool("force", false, "Force upgrade by ignoring errors")
+	flags.StringArray("finalizer-env", []string{},
+		"Set finalizer environment in the format key=value.")
+	flags.Bool("preserve-system-essentials", true, "Preserve system luet files")
+	flags.BoolP("yes", "y", false, "Don't ask questions")
+	flags.Bool("download-only", false, "Download only")
+	flags.Bool("skip-finalizers", false,
 		"Skip the execution of the finalizers.")
-	upgradeCmd.Flags().Bool("sync-repos", false,
+	flags.Bool("sync-repos", false,
 		"Sync repositories before upgrade. Note: If there are in memory repositories then the sync is done always.")
 
 	return upgradeCmd
