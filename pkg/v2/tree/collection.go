f34d2ec7fb6ec07d8873fd7ddb945e556563b413
Daniele Rondina <geaaru@gmail.com>
2023-10-24 07:40:50 +0200

    v2/tree: Add struct CollectionRender to read general map from collection.yaml

--- pkg/v2/tree/collection.go
+++ pkg/v2/tree/collection.go
@@ -14,11 +14,11 @@ import (
 	pkg "github.com/geaaru/luet/pkg/package"
 )
 
-func ReadCollectionFile(cFile string) (*pkg.Collection, error) {
-	ans := &pkg.Collection{
-		Packages: []pkg.DefaultPackage{},
-	}
+type CollectionRender struct {
+	Packages []map[string]interface{} `json:"packages" yaml:"packages"`
+}
 
+func ReadCollectionFile(cFile string) (*pkg.Collection, error) {
 	data, err := os.ReadFile(cFile)
 	if err != nil {
 		return nil, errors.New(
@@ -26,9 +26,69 @@ func ReadCollectionFile(cFile string) (*pkg.Collection, error) {
 				cFile, err.Error()))
 	}
 
-	if err := yaml.Unmarshal(data, ans); err != nil {
+	return NewCollection(&data)
+}
+
+func NewCollection(data *[]byte) (*pkg.Collection, error) {
+	ans := &pkg.Collection{
+		Packages: []pkg.DefaultPackage{},
+	}
+
+	if err := yaml.Unmarshal(*data, ans); err != nil {
 		return nil, err
 	}
 
 	return ans, nil
+}
+
+func ReadCollectionFileLoad(cFile string) (*CollectionRender, error) {
+	data, err := os.ReadFile(cFile)
+	if err != nil {
+		return nil, errors.New(
+			fmt.Sprintf("Error on read file %s: %s",
+				cFile, err.Error()))
+	}
+
+	return NewCollectionRender(&data)
+}
+
+func NewCollectionRender(data *[]byte) (*CollectionRender, error) {
+	ans := &CollectionRender{
+		Packages: []map[string]interface{}{},
+	}
+
+	if err := yaml.Unmarshal(*data, ans); err != nil {
+		return nil, err
+	}
+
+	return ans, nil
+}
+
+func (c *CollectionRender) GetPackageValues(p *pkg.DefaultPackage) (*map[string]interface{}, error) {
+	gpSelector, err := p.ToGentooPackage()
+	if err != nil {
+		return nil, err
+	}
+
+	for idx, _ := range c.Packages {
+		name, _ := c.Packages[idx]["name"].(string)
+		cat, _ := c.Packages[idx]["category"].(string)
+		version, _ := c.Packages[idx]["version"].(string)
+
+		cPkg := pkg.NewPackageWithCatThin(cat, name, version)
+
+		if p.AtomMatches(cPkg) {
+			gp, _ := cPkg.ToGentooPackage()
+			admitted, err := gpSelector.Admit(gp)
+			if err != nil {
+				return nil, err
+			}
+
+			if admitted {
+				return &c.Packages[idx], nil
+			}
+		}
+	}
+
+	return nil, nil
 }
