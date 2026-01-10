aeeebc322372d3f9fb9c3a43b79e72dcc83e64e2
Daniele Rondina <geaaru@macaronios.org>
2026-01-10 22:31:32 +0100

    tests/integration: Disable all tests for now

---
+++ tests/integration/disable_34_annotations.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+testsourcedir=$(dirname "${BASH_SOURCE[0]}")
+source ${testsourcedir}/_common.sh
+
+oneTimeSetUp() {
+  export tmpdir="$(mktemp -d)"
+}
+
+oneTimeTearDown() {
+  rm -rf "$tmpdir"
+}
+
+testBuild() {
+  $LUET_BUILD tree genidx --only-upper-level -t "$ROOT_DIR/tests/fixtures/annotations"
+  genidx=$?
+  assertEquals 'genidx successfully' "$genidx" "0"
+
+  mkdir $tmpdir/testbuild
+  $LUET_BUILD build --tree "$ROOT_DIR/tests/fixtures/annotations" --destination $tmpdir/testbuild --compression gzip test/pkgA test/pkgB > ${OUTPUT}
+  buildst=$?
+  assertEquals 'builds successfully' "$buildst" "0"
+  assertTrue 'create package A' "[ -e '$tmpdir/testbuild/pkgA-test-0.1.package.tar.gz' ]"
+  assertTrue 'create package B' "[ -e '$tmpdir/testbuild/pkgB-test-0.1.package.tar.gz' ]"
+}
+
+testRepo() {
+  assertTrue 'no repository' "[ ! -e '$tmpdir/testbuild/repository.yaml' ]"
+  $LUET_BUILD create-repo --tree "$ROOT_DIR/tests/fixtures/annotations" \
+  --output $tmpdir/testbuild \
+  --packages $tmpdir/testbuild \
+  --name "test" \
+  --descr "Test Repo" \
+  --urls $tmpdir/testrootfs \
+  --type disk > ${OUTPUT}
+
+  createst=$?
+  assertEquals 'create repo successfully' "$createst" "0"
+  assertTrue 'create repository' "[ -e '$tmpdir/testbuild/repository.yaml' ]"
+}
+
+testConfig() {
+    mkdir $tmpdir/testrootfs
+    cat <<EOF > $tmpdir/luet.yaml
+general:
+  debug: true
+system:
+  rootfs: $tmpdir/testrootfs
+  database_path: "/"
+  database_engine: "boltdb"
+config_from_host: true
+repositories:
+   - name: "main"
+     type: "disk"
+     enable: true
+     cached: true
+     urls:
+       - "$tmpdir/testbuild"
+EOF
+    $LUET config --config $tmpdir/luet.yaml
+    res=$?
+    assertEquals 'config test successfully' "$res" "0"
+}
+
+testInstall() {
+    $LUET install --sync-repos -y --config $tmpdir/luet.yaml test/pkgA test/pkgB
+    installst=$?
+    assertEquals 'install test successfully' "$installst" "0"
+    assertTrue 'package A installed' "[ -e '$tmpdir/testrootfs/file1' ]"
+    assertTrue 'package B installed' "[ -e '$tmpdir/testrootfs/file2' ]"
+}
+
+testReInstall() {
+    output=$($LUET install --sync-repos -y --config $tmpdir/luet.yaml  test/c@1.0)
+    installst=$?
+    assertEquals 'install test successfully' "$installst" "0"
+    assertContains 'contains warning' "$output" 'No packages to install'
+}
+
+testUnInstall() {
+    $LUET uninstall -y --config $tmpdir/luet.yaml test/c
+    installst=$?
+    assertEquals 'uninstall test successfully' "$installst" "0"
+    assertTrue 'package uninstalled' "[ ! -e '$tmpdir/testrootfs/c' ]"
+}
+
+testInstallAgain() {
+    assertTrue 'package uninstalled' "[ ! -e '$tmpdir/testrootfs/c' ]"
+    output=$($LUET install --sync-repos -y --config $tmpdir/luet.yaml test/c@1.0)
+    installst=$?
+    assertEquals 'install test successfully' "$installst" "0"
+    assertNotContains 'contains warning' "$output" 'No packages to install'
+    assertTrue 'package installed' "[ -e '$tmpdir/testrootfs/c' ]"
+    assertTrue 'package in cache' "[ -e '$tmpdir/testrootfs/packages/c-test-1.0.package.tar.gz' ]"
+}
+
+testCleanup() {
+    $LUET cleanup --config $tmpdir/luet.yaml
+    installst=$?
+    assertEquals 'install test successfully' "$installst" "0"
+    assertTrue 'package installed' "[ ! -e '$tmpdir/testrootfs/packages/c-test-1.0.package.tar.gz' ]"
+}
+
+# Load shUnit2.
+. "$ROOT_DIR/tests/integration/shunit2"/shunit2
+
