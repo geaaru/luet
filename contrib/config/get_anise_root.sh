a8bc723d48917ec95860d5feec3da67b7fb2de08
Daniele Rondina <geaaru@macaronios.org>
2026-01-10 22:37:40 +0100

    contrib: Rename scripts and configs to anise

--- contrib/config/get_anise_root.sh
+++ contrib/config/get_anise_root.sh
@@ -5,45 +5,45 @@ if [ $(id -u) -ne 0 ]
 fi
 
 set -ex
-export LUET_NOLOCK=true
+export ANISE_NOLOCK=true
 
-GITHUB_USER="${GITHUB_USER:-geaaru}"
+GITHUB_USER="${GITHUB_USER:-macaroni-os}"
 GITHUB_BRANCH="${GITHUB_BRANCH:-geaaru}"
 
-LUET_VERSION="v0.40.0-${GITHUB_USER}"
-LUET_ROOTFS=${LUET_ROOTFS:-/}
-LUET_DATABASE_PATH=${LUET_DATABASE_PATH:-/var/cache/luet}
-LUET_DATABASE_ENGINE=${LUET_DATABASE_ENGINE:-boltdb}
-LUET_CONFIG_PROTECT=${LUET_CONFIG_PROTECT:-1}
-LUET_ARCH=${LUET_ARCH:-x86_64}
+ANISE_VERSION="v0.41.0-${GITHUB_USER}"
+ANISE_ROOTFS=${ANISE_ROOTFS:-/}
+ANISE_DATABASE_PATH=${ANISE_DATABASE_PATH:-/var/cache/anise}
+ANISE_DATABASE_ENGINE=${ANISE_DATABASE_ENGINE:-boltdb}
+ANISE_CONFIG_PROTECT=${ANISE_CONFIG_PROTECT:-1}
+ANISE_ARCH=${ANISE_ARCH:-x86_64}
 
-curl -L https://github.com/${GITHUB_USER}/luet/releases/download/${LUET_VERSION}/luet-${LUET_VERSION}-Linux-${LUET_ARCH} --output /usr/bin/anise
+curl -L https://github.com/${GITHUB_USER}/anise/releases/download/${ANISE_VERSION}/anise-${ANISE_VERSION}-Linux-${ANISE_ARCH} --output /usr/bin/anise
 chmod +x /usr/bin/anise
 
-mkdir -p /etc/luet/repos.conf.d || true
-mkdir -p $LUET_DATABASE_PATH || true
-mkdir -p /var/tmp/luet || true
+mkdir -p /etc/anise/repos.conf.d || true
+mkdir -p $ANISE_DATABASE_PATH || true
+mkdir -p /var/tmp/anise || true
 
-if [ "${LUET_CONFIG_PROTECT}" = "1" ] ; then
-  mkdir -p /etc/luet/config.protect.d || true
-  curl -L https://raw.githubusercontent.com/${GITHUB_USER}/luet/${GITHUB_BRANCH}/contrib/config/config.protect.d/01_etc.yml.example --output /etc/luet/config.protect.d/01_etc.yml
+if [ "${ANISE_CONFIG_PROTECT}" = "1" ] ; then
+  mkdir -p /etc/anise/config.protect.d || true
+  curl -L https://raw.githubusercontent.com/${GITHUB_USER}/anise/${GITHUB_BRANCH}/contrib/config/config.protect.d/01_etc.yml.example --output /etc/anise/config.protect.d/01_etc.yml
 fi
-curl -L https://raw.githubusercontent.com/geaaru/repo-index/master/packages/geaaru-repo-index.yml --output /etc/luet/repos.conf.d/geaaru-repo-index.yml
+curl -L https://raw.githubusercontent.com/geaaru/repo-index/master/packages/geaaru-repo-index.yml --output /etc/anise/repos.conf.d/geaaru-repo-index.yml
 
-cat > /etc/luet/luet.yaml <<EOF
+cat > /etc/anise/anise.yaml <<EOF
 general:
   debug: false
 system:
-  rootfs: ${LUET_ROOTFS}
-  database_path: "${LUET_DATABASE_PATH}"
-  database_engine: "${LUET_DATABASE_ENGINE}"
-  tmpdir_base: "/var/tmp/luet"
+  rootfs: ${ANISE_ROOTFS}
+  database_path: "${ANISE_DATABASE_PATH}"
+  database_engine: "${ANISE_DATABASE_ENGINE}"
+  tmpdir_base: "/var/tmp/anise"
 EOF
 
-if [ "${LUET_ARCH}" = "x86_64" ] ; then
+if [ "${ANISE_ARCH}" = "x86_64" ] ; then
   anise repo update
   anise install -y repository/mottainai-stable repository/geaaru-repo-index --force
-  anise install --sync-repos -y system/luet-${GITHUB_USER} --force
+  anise install --sync-repos -y system/anise-${GITHUB_USER} --force
 else
   echo "Luet ARM repositories are not available yet."
 fi
